9e0c25e29b4431fe4380b01166276c5682552a0e
glynntucker <46584958+glynntucker@users.noreply.github.com>
2023-04-08 13:34:55 +1000

    updated header

--- header.js
+++ header.js
@@ -1,21 +1,30 @@
 document.write(`
-  <div class="header">
-  <ul>
-  <li><a href="/index">Home</a></li>
-  <li class="dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Section Properties</a>
-  <div class="dropdown-content">
+
+  
+<div class="sidenav">
+
+<a href="/index">Home</a>
+
+<button class="dropdown-btn">Section Properties
+  <i class="fa fa-caret-down"></i>
+</button>
+
+<div class="dropdown-container">
   <a href="/properties/ub">UB</a>
   <a href="/properties/uc">UC</a>
   <a href="/properties/pfc">PFC</a>
   <a href="/properties/rhs">RHS</a>
   <a href="/properties/shs">SHS</a>
   <a href="/properties/chs">CHS</a>
-  </div>
-  </li>
-  <li class="dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Moment Capacities</a>
-  <div class="dropdown-content">
+</div>
+
+
+<button class="dropdown-btn">Moment Capacities
+  <i class="fa fa-caret-down"></i>
+</button>
+
+<div class="dropdown-container">
+
   <a href="/moment/ub">UB</a>
   <a href="/moment/uc">UC</a>
   <a href="/moment/pfc">PFC</a>
@@ -25,11 +34,16 @@ document.write(`
   <a href="/moment/shs450">SHS450</a>
   <a href="/moment/shs250">CHS250</a>
   <a href="/moment/shs350">CHS350</a>
-  </div>
-  </li>
-  <li class="dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Compression Capacities</a>
-  <div class="dropdown-content">
+</div>
+
+
+<button class="dropdown-btn">Compression Capacities
+  <i class="fa fa-caret-down"></i>
+</button>
+
+<div class="dropdown-container">
+
+
   <a href="/comp/ub">UB</a>
   <a href="/comp/uc">UC</a>
   <a href="/comp/pfc">PFC</a>
@@ -39,104 +53,118 @@ document.write(`
   <a href="/comp/shs450">SHS450</a>
   <a href="/comp/shs250">CHS250</a>
   <a href="/comp/shs350">CHS350</a>
-  </div>
-  </li>
-  <li><a href="/properties/bolts">Bolts</a></li>
-  <li><a href="/properties/welds">Welds</a></li>
-  <li><a href="/properties/plate">Plates</a></li>
-  <li class="dropdown">
-  <a href="/conc/concrete" class="dropbtn">Concrete</a>
-  <div class="dropdown-content">
-  <a href="/conc/mesh">Mesh/Steel</a>
-  </div>
-  </li>
-  <li class="headerLi"><a href="/interpolation">Interpolation</a></li>
-  <!--<li class="finalLogo" style="float:right; background-color: #8a8a8a;"><a href="/index"><img src="../GTLOGO.png" align="center" width=200px></a></li>-->
-  <li class="GT SP" style="float:right"><a href="https://glynntucker.sharepoint.com" target="_blanks">Sharepoint Home</a></li>
+</div>
 
-  </ul>
-  <style>
-  ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #282828;
-  }
+<a href="/properties/bolts">Bolts</a>
+<a href="/properties/welds">Welds</a>
+<a href="/plate">Plates</a>
 
-  li {
-    float: left;
-  }
+<a href="/conc/concrete" class="dropbtn">Concrete</a>
 
-  li a, .dropbtn {
-    display: inline-block;
-    color: #969696;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-weight: bold;
-  }
+<a href="/conc/mesh">Mesh/Steel</a>
 
-  li a:hover, .dropdown:hover .dropbtn {
-    -webkit-transition: background-color 0.25s ease-out;
-    transition: background-color 0.25s ease-out;
-    background-color: #084887;
-  }
+<a href="/interpolation">Interpolation</a>
+<!--<li class="finalLogo" style="float:right; background-color: #8a8a8a;"><a href="/index"><img src="../GTLOGO.png" align="center" width=200px></a>-->
+<a href="https://glynntucker.sharepoint.com" target="_blanks">Sharepoint Home</a>
+</div>
 
-  li.dropdown {
-    display: inline-block;
-  }
 
-body {background-color: #3C3C3C;}
+<style>
+.headerbar {
+  background-color: #084887B3;
+  backdrop-filter: blur(10px);
+  text-align: left;
+  padding: 10px 0px 10px 240px;
+}
 
-  .dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: #f9f9f9;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-  }
 
-  .dropdown-content a {
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-    text-align: left;
-  }
+body {
+  font-family: "Lato", sans-serif;
+  margin: 0px;
+  background-color: #b0b0b0;
+}
 
-  .dropdown-content a:hover {
-    background-color: #f1f1f1
-  }
+/* Fixed sidenav, full height */
+.sidenav {
+  height: 100%;
+  width: 200px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
 
-  .dropdown:hover .dropdown-content {
-    display: block;
-  }
+/* Style the sidenav links and the dropdown button */
+.sidenav a, .dropdown-btn {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 20px;
+  color: #818181;
+  display: block;
+  border: none;
+  background: none;
+  width: 100%;
+  text-align: left;
+  cursor: pointer;
+  outline: none;
+}
 
-  li.finalLogo a:hover {
-    -webkit-transition: background-color 2s ease-out;
-    transition: background-color 2s ease-out;
-    background-color: #ffffff;
-  }
+/* On mouse-over */
+.sidenav a:hover, .dropdown-btn:hover {
+  color: #f1f1f1;
+}
 
-  .footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    background-color: #2f2f2f;
-    color: white;
-    text-align: center;
-  }
+/* Main content */
+.main {
+  margin-left: 270px; /* Same as the width of the sidenav */
+  font-size: 20px; /* Increased text to enable scrolling */
+  padding: 0px 0px;
+}
 
-  .rotateOrder div {
-    -webkit-transform: rotate(-90.0deg);
-    position: absolute;
-    vertical-align: middle;
-    padding-top: 5px;
-    width:10px;
-  }
-  </style>
-  </div>
+/* Add an active class to the active dropdown button */
+.active {
+  background-color: #084887B3;
+  color: white;
+}
+
+/* Dropdown container (hidden by default). Optional: add a lighter background color and some left padding to change the design of the dropdown content */
+.dropdown-container {
+  display: none;
+  background-color: #262626;
+  padding-left: 8px;
+}
+
+/* Optional: Style the caret down icon */
+.fa-caret-down {
+  float: right;
+  padding-right: 8px;
+}
+
+/* Some media queries for responsiveness */
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+</style>
+<script>
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+</script>
+
   `);
